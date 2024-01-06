abd348c1d2b48b9da865aa1f587dd8652bae1050
Kevin <epickillerdude2007@gmail.com>
2024-01-06 10:35:56 -0500

    Made score show on every turn

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let playerPoints = 0
+let computerPoints = 0
+
+
 function getcomputerScore() {
     compScore = Math.random()
     if (compScore < 0.37) {
@@ -13,54 +17,65 @@ function getcomputerScore() {
         return("paper")
     }
 }    
+
 function playRound(userinput, computerScore) {
     
     
     if (computerScore == userinput) {
-        
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         return("tie")
 
-    } 
+    }   
     else if (userinput === "rock" && computerScore === "scissors") {
         playerPoints++
-        
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         return("win")
         
     } 
     else if (userinput === "rock" && computerScore === "paper") {
         computerPoints++
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         
         return("loss")
     }
     else if (userinput === "paper" && computerScore === "scissors") {
         computerPoints++
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         
         return("loss")
     } 
     else if (userinput === "paper" && computerScore === "rock") {
         playerPoints++
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         
         return("win")
     }
     else if (userinput === "scissors" && computerScore === "rock") {
         computerPoints++
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         
         return("loss")
     } 
     else if (userinput === "scissors" && computerScore === "paper") {
         playerPoints++
+        console.log("You have " + playerPoints)
+        console.log("Opponent has " +computerPoints)
         return("win")
     }
 }
 
 
-function game() {
-    let playerPoints = 0
-    let computerPoints = 0
 
+function game() {
     
-    for(let i = 0; i < 5; i++) {
-        
+    
+    for(let i = 0; i < 2; i++) {
         const userinput = prompt("Rock paper or scissors").toLowerCase()
         const computerScore = getcomputerScore()
         console.log(userinput)
@@ -75,6 +90,12 @@ function game() {
         console.log("You tied")
     }
 }
+
+const rockBtn = document.createElement('button')
+const paperBtn = document.createElement('button')
+const scissorsBtn = document.createElement('button')
+
+
 
 game()
 
